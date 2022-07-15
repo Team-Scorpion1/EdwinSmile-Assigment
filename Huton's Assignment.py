8e568268e3a219ecab3d06887464b0cc9dd445aa
Your Name <you@example.com>
2022-07-15 16:26:53 +0000

    adding my assignment

---
+++ Huton's Assignment.py
@@ -0,0 +1,5 @@
+name = input("Please enter your name:")
+age= input("Please enter your age:")
+def getyear(age):
+    return 2022 - int(age)
+print("Hello,", name + ", please you were born in the year",+ getyear(age),".Thank you.") 
